3be1abaad1b75e9c4487f58bbd8fb1b83a0badd7
Alexandre Rio <alexandre.rio@irisa.fr>
2015-07-28 10:38:50 +0200

    hand crafted test

--- org.kevoree.modeling.c.generator/src/main/resources/hardcoded/main.c
+++ org.kevoree.modeling.c.generator/src/main/resources/hardcoded/main.c
@@ -4,31 +4,48 @@
 #include <stdlib.h>
 #include <check.h>
 
-START_TEST (test1)
+START_TEST (metaClassTest)
 {
-  ContainerRoot *r = new_ContainerRoot();
   Group *g = new_Group();
-  printf("%s id: %s key: %s\n", r->VT->metaClassName(r), r->generated_KMF_ID, r->VT->internalGetKey(r));
-  g->name = "test";
-  printf("%s key: %s\n", g->VT->metaClassName(g), g->VT->internalGetKey(g));
-  r->VT->containerRootAddGroups(r, g);
-  Group *get = r->VT->containerRootFindgroupsByID(r, "test");
-  printf("%s key: %s\n", get->VT->metaClassName(get), get->VT->internalGetKey(get));
-  ck_assert_str_eq(get->VT->metaClassName(get), "test");
+
+  ck_assert_str_eq(g->VT->metaClassName(g), "Group");
 }
 END_TEST
 
-Suite * money_suite(void)
+START_TEST (nameTest)
+{
+  Group *g = new_Group();
+  g->name = "test";
+
+  ck_assert_str_eq(g->VT->internalGetKey(g), "test");
+}
+END_TEST
+
+START_TEST (findByIdTest)
+{
+  ContainerRoot *r = new_ContainerRoot();
+  Group *g = new_Group();
+  g->name = "test";
+  r->VT->containerRootAddGroups(r, g);
+  Group *get = r->VT->containerRootFindgroupsByID(r, "test");
+
+  ck_assert_str_eq(get->VT->internalGetKey(get), g->VT->internalGetKey(g));
+}
+END_TEST
+
+Suite * containerroot_suite(void)
 {
   Suite *s;
   TCase *tc_core;
 
-  s = suite_create("Money");
+  s = suite_create("ContainerRoot");
 
   /* Core test case */
   tc_core = tcase_create("Core");
 
-  tcase_add_test(tc_core, test1);
+  tcase_add_test(tc_core, metaClassTest);
+  tcase_add_test(tc_core, nameTest);
+  tcase_add_test(tc_core, findByIdTest);
   suite_add_tcase(s, tc_core);
 
   return s;
@@ -36,12 +53,11 @@ Suite * money_suite(void)
 
 int main(void)
 {
-
   int number_failed;
   Suite *s;
   SRunner *sr;
 
-  s = money_suite();
+  s = containerroot_suite();
   sr = srunner_create(s);
 
   srunner_run_all(sr, CK_NORMAL);
