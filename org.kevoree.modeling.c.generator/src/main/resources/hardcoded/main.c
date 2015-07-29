28df7bd7c0c0a0045d4fae63831cde2cfb98b420
Alexandre Rio <alexandre.rio@irisa.fr>
2015-07-29 17:40:04 +0200

    detect a bug

--- org.kevoree.modeling.c.generator/src/main/resources/hardcoded/main.c
+++ org.kevoree.modeling.c.generator/src/main/resources/hardcoded/main.c
@@ -1,7 +1,19 @@
+#include "ContainerRoot.h"
+#include "Group.h"
+#include "MessagePortType.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(void)
 {
-    printf("coucou");
+    ContainerRoot *r = new_ContainerRoot();
+    printf("root %s\n", r->VT->internalGetKey(r));
+    Group *g = new_Group();
+    printf("group %s\n", g->VT->internalGetKey(g));
+    g->name = "my_name";
+    printf("group %s\n", g->VT->internalGetKey(g));
+
+    MessagePortType *m = new_MessagePortType();
+    printf("msgPT %s\n", m->VT->internalGetKey(m));
 }
