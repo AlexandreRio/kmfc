f46ed243d72595d76e5eee23926f281ced86884a
Alexandre Rio <alexandre.rio@irisa.fr>
2015-08-06 18:41:15 +0200

    work in progress

--- org.kevoree.modeling.c.generator/src/main/resources/hardcoded/main.c
+++ org.kevoree.modeling.c.generator/src/main/resources/hardcoded/main.c
@@ -1,4 +1,5 @@
 #include "ContainerRoot.h"
+#include "Group.h"
 #include "VisitorJSON.h"
 
 #include <stdlib.h>
@@ -6,10 +7,19 @@
 
 int main(void)
 {
-	ContainerRoot *o = new_ContainerRoot();
-	o->VT->delete(o);
-	VisitorJSON* visitor = visitor_json_new();
-	visitor->visitor->visit(visitor, o->VT->metaClassName(o), o);
-	visitor->destroy(visitor);
-	free(o);
+  ContainerRoot *o = new_ContainerRoot();
+  Group* g1 = new_Group();
+  g1->name = "some_group_name";
+  o->VT->containerRootAddGroups(o, g1);
+
+  Group* g2 = new_Group();
+  g2->name = "other_group";
+  o->VT->containerRootAddGroups(o, g2);
+
+  VisitorJSON* visitor = visitor_json_new();
+  //visitor->visitor->visit(visitor, o->VT->metaClassName(o), o);
+  o->VT->fptrAccept(o, visitor);
+  visitor->destroy(visitor);
+  o->VT->delete(o);
+  free(o);
 }
--- org.kevoree.modeling.c.generator/src/main/resources/hardcoded/main_manual.c
+++ org.kevoree.modeling.c.generator/src/main/resources/hardcoded/main_manual.c
@@ -1,3 +1,4 @@
+#include "ContainerRoot.h"
 #include "Group.h"
 
 #include <stdlib.h>
@@ -5,19 +6,24 @@
 
 int main(void)
 {
-//  Group *o = new_Group();
-//  char* str = "my_str";
-//  o->internalKey = str;
-//  printf("%s\n", o->internalKey);
-//  printf("%p\n", o->VT->namedElementRemoveInternalKey);
-//  printf("%p\n", o->VT->instanceAddStarted);
-//  bool b = true;
-//  printf("started: %d\n", o->started);
-//  o->VT->instanceAddStarted((Instance*)o, &b);
-//  printf("started: %d\n", o->started);
-//  printf("name: %s\n", o->name);
-//  o->VT->namedElementAddName(o, "salut");
-//  printf("name: %s\n", o->name);
-//  //o->VT->namedElementRemoveInternalKey((NamedElement*)o, str);
+  ContainerRoot* o = new_ContainerRoot();
 
+  Group* g1 = new_Group();
+  g1->name = "some_group_name";
+  o->VT->containerRootAddGroups(o, g1);
+
+  Group* g2 = new_Group();
+  g2->name = "other_group";
+  o->VT->containerRootAddGroups(o, g2);
+
+  /** reusing code */
+  hashmap_map* m = (hashmap_map*) o->groups;
+  for (int i=0; i<m->table_size; i++)
+  {
+    Group* data = (Group*) (m->data[i].data);
+    printf("%s: %s\n", data->VT->metaClassName(data), data->VT->internalGetKey(data));
+  }
+
+
+  printf("size: %d", hashmap_length(o->groups));
 }
