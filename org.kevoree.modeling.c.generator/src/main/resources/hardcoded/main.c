d2b7eccb5cfb2397db44545a45279fef4fb67c96
Alexandre Rio <alexandre.rio@irisa.fr>
2015-08-14 12:02:20 +0200

    loop over the json string using jsonparse

--- org.kevoree.modeling.c.generator/src/main/resources/hardcoded/main.c
+++ org.kevoree.modeling.c.generator/src/main/resources/hardcoded/main.c
@@ -1,21 +1,61 @@
 #include "ContainerRoot.h"
-#include "Group.h"
+
+#include "json.h"
+#include "jsonparse.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 
+void parse(struct jsonparse_state *state, char type)
+{
+  char str[200];
+
+  while (type != JSON_TYPE_ERROR)
+  {
+    type = jsonparse_next(state);
+    jsonparse_copy_value(state, str, sizeof str);
+    printf("%s\n", str);
+  }
+}
+
 int main(void)
 {
+  FILE* model_file = fopen("../5nodes1component.json", "r");
+  if (model_file == NULL)
+    return EXIT_FAILURE;
+
+  fseek(model_file, 0L, SEEK_END);
+  int size = ftell(model_file);
+  fseek(model_file, 0L, SEEK_SET);
+
+  char* model = malloc(size+1);
+  int ch;
+  bool firstChar = true;
+
+  while ((ch = fgetc(model_file)) != EOF)
+  {
+    if (firstChar)
+    {
+      sprintf(model, "%c", ch);
+      firstChar = false;
+    } else
+    {
+      sprintf(model, "%s%c", model, ch);
+    }
+  }
+  fclose(model_file);
+
+  struct jsonparse_state state;
+  jsonparse_setup(&state, model, size+1);
+
+  parse(&state, jsonparse_next(&state));
+
   ContainerRoot *o = new_ContainerRoot();
-  Group* g1 = new_Group();
-  g1->name = "some_group_name";
-  o->VT->containerRootAddGroups(o, g1);
 
-  Group* g2 = new_Group();
-  g2->name = "other_group";
-  o->VT->containerRootAddGroups(o, g2);
-
-  o->VT->fptrToJSON(o);
+  //o->VT->fptrToJSON(o);
   o->VT->delete(o);
+  free(model);
   free(o);
+
+  return EXIT_SUCCESS;
 }
